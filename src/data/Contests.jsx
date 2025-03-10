c2182f8f2d5b220227e00fa911aad984e89935b6
Felipe Focil <felipe.focil@omegaup.com>
2025-03-10 00:28:39 -0600

    Modify contest

--- src/data/Contests.jsx
+++ src/data/Contests.jsx
@@ -4,100 +4,27 @@ import EGOI24 from "../assets/EGOI24.jpg";
 import OFMI22 from "../assets/OFMI22.jpg";
 
 export const Contests = {
-  "OFMI2025":{
-    title: "Cuarta OFMI",
-    organizer: "OFMI",
+  "MEI2025":{
+    title: "Tercer Maraton de Programacion Competitiva UPAEP 2025",
+    organizer: "CPU",
     img: OFMI22,
-    content: "Examen Nacional de la Olimpiada Femenil Mexicana de Informatica 2024-2025",
-    alternative : "OFMI 2025",
+    content: "Maraton de Programacion Competititva 24 horas UPAEP 2025",
+    alternative : "MEI 2025",
     links : [
-      "https://omegaup.com/arena/4AOFMIDIA1/scoreboard/QV3gsHbzdYMJjrfwx64crNBeaRDc4V",
-      "https://omegaup.com/arena/4AOFMIDIA2/scoreboard/fhEKmK7tfscm5ZEH9XLQ3Uzw5K8tqj"
+        "https://omegaup.com/arena/maraton2025/scoreboard/QNADEzUrthtQmpgZ4hbEMAtjtVdrt9",
+        "https://omegaup.com/arena/maraton20252/scoreboard/QcVGDqSwahjEnJU2WJept9tQ2Qxrjg",
+        "https://omegaup.com/arena/maraton20253/scoreboard/hdtMu2gzMZQJWUVVkqtCCtBLQ3ywA8",
+        "https://omegaup.com/arena/maraton20254/scoreboard/FR2rsB6hQk4VcvzJYQZMML5h84nmES",
+        "https://omegaup.com/arena/maraton20255/scoreboard/89dW4LEbYggDmfbUbpfBWpgtk7z4dR"
     ],
     config : {
-      gold : 0,
-      silver : 0,
-      bronze : 0,
-      criteria : "points",
-      diploma : true,
-    }
-  },
-  "EGOI2024":{
-    title: "Selectivo EGOI 2024",
-    organizer: "OFMI",
-    img: EGOI24,
-    content: "Exámen Selectivo de la delgación mexicana en la European Girls Olympiad of Informatics 2024 en Paises Bajos",
-    alternative : "EGOI 2024",
-    links : [
-      "https://omegaup.com/arena/Selectivo-EGOI-2024-1/scoreboard/LWxMSydqWEXwKcGjByxHMGCpyWxLsp",
-      "https://omegaup.com/arena/Selectivo-EGOI-2024-2/scoreboard/7kG2mSUG7ad5VV3yntuBTVesTPC3fb",
-      "https://omegaup.com/arena/Selectivo-EGOI-2024-3/scoreboard/pPWtvATCqfefHbLDvsXpZMRmye6qPF"
-
-    ],
-    config : {
-      gold : 4,
-      silver : 0,
-      bronze : 0,
-      criteria : "medals",
-      diploma : false,
-    }
-  },
-  "OFMI2023":{
-    title: "Tercera OFMI",
-    organizer: "OFMI",
-    img: OFMI22,
-    content: "Examen Nacional de la Olimpiada Femenil Mexicana de Informatica 2023-2024",
-    alternative : "OFMI 2023",
-    links : [
-      "https://omegaup.com/arena/3AOFMI/scoreboard/LS723aAUUCVTUWAHPLyJPE8QCxPXwx",
-      "https://omegaup.com/arena/3aOFMIDIA2/scoreboard/CDkr2xsnBZB82NKjf2LjhML566mBGd"
-    ],
-    config : {
-      gold : 445,
-      silver : 300,
-      bronze : 135,
+      gold : 1,
+      silver : 1,
+      bronze : 1,
       criteria : "points",
       diploma : false,
     }
-  },
-  "EGOI2023":{
-    title: "Selectivo EGOI 2023",
-    organizer: "OFMI",
-    img: EGOI23,
-    content: "Exámen Selectivo de la delgación mexicana en la European Girls Olympiad of Informatics 2023 en Suecia",
-    alternative : "EGOI 2023",
-    links : [
-      "https://omegaup.com/arena/Selectivo-EGOI-2023-1/scoreboard/MgE6RF3DuPKjXuCV9rhxkpLsGNKwNP",
-      "https://omegaup.com/arena/Selectivo-EGOI-2023-2/scoreboard/JLhh4dr5dgsan98nkKcAaBX54Sxfsx",
-      "https://omegaup.com/arena/Selectivo-EGOI-2023-3/scoreboard/jAPMmZMzMeS2ZeCt9nYRehbSLLzL5d"
-
-    ],
-    config : {
-      gold : 4,
-      silver : 0,
-      bronze : 0,
-      criteria : "medals",
-      diploma : false,
-    }
-  },
-  "OFMI2022":{
-    title: "Segunda OFMI",
-    organizer: "OFMI",
-    img: EGOI22,
-    content: "Examen Nacional de la Olimpiada Femenil Mexicana de Informatica 2022-2023",
-    alternative : "OFMI 2022",
-    links : [
-      "https://omegaup.com/arena/OFMI2023DIA1/scoreboard/Z4aTTx9FW9TBhvptr4dAmGrFkf98M2",
-      "https://omegaup.com/arena/OFMI2023DIA2/scoreboard/YKvJVc7qcSZUXFXs6EJCTErTtHREBE"
-    ],
-    config : {
-      gold : 415,
-      silver : 323,
-      bronze : 265,
-      criteria : "points",
-      diploma : false,
-    }
-  },
+  }
   
   
 };
